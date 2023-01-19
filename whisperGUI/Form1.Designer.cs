f781ba5493de749a71abc163f571624565b0f330
XTer <82741331+X-T-E-R@users.noreply.github.com>
2023-01-20 01:03:49 +0800

    Add files via upload

---
+++ whisperGUI/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿
+namespace whisperGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clearQueue = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ComboLan = new System.Windows.Forms.ComboBox();
+            this.ComboModel = new System.Windows.Forms.ComboBox();
+            this.clearList = new System.Windows.Forms.Button();
+            this.cmdPopCheck = new System.Windows.Forms.CheckBox();
+            this.saveExtentionSelect = new System.Windows.Forms.CheckedListBox();
+            this.autoClear = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // clearQueue
+            // 
+            this.clearQueue.Location = new System.Drawing.Point(26, 172);
+            this.clearQueue.Name = "clearQueue";
+            this.clearQueue.Size = new System.Drawing.Size(170, 47);
+            this.clearQueue.TabIndex = 0;
+            this.clearQueue.Text = "clear queue";
+            this.clearQueue.UseVisualStyleBackColor = true;
+            this.clearQueue.Click += new System.EventHandler(this.clearQueue_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "音视频文件|*.mp4;*.mp3;*.mkv;*.flac;*.wav|所有文件|*.*";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("等线", 14F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(26, 239);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(834, 47);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Click += new System.EventHandler(this.comboBox1_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(689, 32);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(171, 168);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 320);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 90;
+            this.dataGridView1.RowTemplate.Height = 37;
+            this.dataGridView1.Size = new System.Drawing.Size(834, 699);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // ComboLan
+            // 
+            this.ComboLan.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.ComboLan.Font = new System.Drawing.Font("等线", 14F);
+            this.ComboLan.FormattingEnabled = true;
+            this.ComboLan.Items.AddRange(new object[] {
+            "Chinese",
+            "English",
+            "Japanese",
+            "Afrikaans",
+            "Albanian",
+            "Amharic",
+            "Arabic",
+            "Armenian",
+            "Assamese",
+            "Azerbaijani",
+            "Bashkir",
+            "Basque",
+            "Belarusian",
+            "Bengali",
+            "Bosnian",
+            "Breton",
+            "Bulgarian",
+            "Burmese",
+            "Castilian",
+            "Catalan",
+            "Chinese",
+            "Croatian",
+            "Czech",
+            "Danish",
+            "Dutch",
+            "English",
+            "Estonian",
+            "Faroese",
+            "Finnish",
+            "Flemish",
+            "French",
+            "Galician",
+            "Georgian",
+            "German",
+            "Greek",
+            "Gujarati",
+            "Haitian",
+            "Haitian Creole",
+            "Hausa",
+            "Hawaiian",
+            "Hebrew",
+            "Hindi",
+            "Hungarian",
+            "Icelandic",
+            "Indonesian",
+            "Italian",
+            "Japanese",
+            "Javanese",
+            "Kannada",
+            "Kazakh",
+            "Khmer",
+            "Korean",
+            "Lao",
+            "Latin",
+            "Latvian",
+            "Letzeburgesch",
+            "Lingala",
+            "Lithuanian",
+            "Luxembourgish",
+            "Macedonian",
+            "Malagasy",
+            "Malay",
+            "Malayalam",
+            "Maltese",
+            "Maori",
+            "Marathi",
+            "Moldavian",
+            "Moldovan",
+            "Mongolian",
+            "Myanmar",
+            "Nepali",
+            "Norwegian",
+            "Nynorsk",
+            "Occitan",
+            "Panjabi",
+            "Pashto",
+            "Persian",
+            "Polish",
+            "Portuguese",
+            "Punjabi",
+            "Pushto",
+            "Romanian",
+            "Russian",
+            "Sanskrit",
+            "Serbian",
+            "Shona",
+            "Sindhi",
+            "Sinhala",
+            "Sinhalese",
+            "Slovak",
+            "Slovenian",
+            "Somali",
+            "Spanish",
+            "Sundanese",
+            "Swahili",
+            "Swedish",
+            "Tagalog",
+            "Tajik",
+            "Tamil",
+            "Tatar",
+            "Telugu",
+            "Thai",
+            "Tibetan",
+            "Turkish",
+            "Turkmen",
+            "Ukrainian",
+            "Urdu",
+            "Uzbek",
+            "Valencian",
+            "Vietnamese",
+            "Welsh",
+            "Yiddish",
+            "Yoruba"});
+            this.ComboLan.Location = new System.Drawing.Point(26, 32);
+            this.ComboLan.Name = "ComboLan";
+            this.ComboLan.Size = new System.Drawing.Size(348, 47);
+            this.ComboLan.TabIndex = 4;
+            this.ComboLan.SelectedIndexChanged += new System.EventHandler(this.ComboLan_SelectedIndexChanged);
+            // 
+            // ComboModel
+            // 
+            this.ComboModel.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.ComboModel.Font = new System.Drawing.Font("等线", 14F);
+            this.ComboModel.FormattingEnabled = true;
+            this.ComboModel.Items.AddRange(new object[] {
+            "tiny",
+            "base",
+            "small",
+            "medium",
+            "large"});
+            this.ComboModel.Location = new System.Drawing.Point(26, 100);
+            this.ComboModel.Name = "ComboModel";
+            this.ComboModel.Size = new System.Drawing.Size(348, 47);
+            this.ComboModel.TabIndex = 5;
+            this.ComboModel.SelectedIndexChanged += new System.EventHandler(this.ComboModel_SelectedIndexChanged);
+            // 
+            // clearList
+            // 
+            this.clearList.Location = new System.Drawing.Point(202, 172);
+            this.clearList.Name = "clearList";
+            this.clearList.Size = new System.Drawing.Size(172, 47);
+            this.clearList.TabIndex = 6;
+            this.clearList.Text = "clear list";
+            this.clearList.UseVisualStyleBackColor = true;
+            this.clearList.Click += new System.EventHandler(this.clearList_Click);
+            // 
+            // cmdPopCheck
+            // 
+            this.cmdPopCheck.AutoSize = true;
+            this.cmdPopCheck.Location = new System.Drawing.Point(412, 148);
+            this.cmdPopCheck.Name = "cmdPopCheck";
+            this.cmdPopCheck.Size = new System.Drawing.Size(198, 28);
+            this.cmdPopCheck.TabIndex = 7;
+            this.cmdPopCheck.Text = "silent pop-up";
+            this.cmdPopCheck.UseVisualStyleBackColor = true;
+            this.cmdPopCheck.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // saveExtentionSelect
+            // 
+            this.saveExtentionSelect.FormattingEnabled = true;
+            this.saveExtentionSelect.Items.AddRange(new object[] {
+            ".srt",
+            ".vtt",
+            ".txt"});
+            this.saveExtentionSelect.Location = new System.Drawing.Point(412, 32);
+            this.saveExtentionSelect.Name = "saveExtentionSelect";
+            this.saveExtentionSelect.Size = new System.Drawing.Size(117, 100);
+            this.saveExtentionSelect.TabIndex = 8;
+            this.saveExtentionSelect.SelectedIndexChanged += new System.EventHandler(this.saveExtentionSelect_SelectedIndexChanged);
+            // 
+            // autoClear
+            // 
+            this.autoClear.AutoSize = true;
+            this.autoClear.Location = new System.Drawing.Point(412, 182);
+            this.autoClear.Name = "autoClear";
+            this.autoClear.Size = new System.Drawing.Size(270, 28);
+            this.autoClear.TabIndex = 9;
+            this.autoClear.Text = "autoclear bat files";
+            this.autoClear.UseVisualStyleBackColor = true;
+            this.autoClear.CheckedChanged += new System.EventHandler(this.autoClear_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 1081);
+            this.Controls.Add(this.autoClear);
+            this.Controls.Add(this.saveExtentionSelect);
+            this.Controls.Add(this.cmdPopCheck);
+            this.Controls.Add(this.clearList);
+            this.Controls.Add(this.ComboModel);
+            this.Controls.Add(this.ComboLan);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.clearQueue);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clearQueue;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox ComboLan;
+        private System.Windows.Forms.ComboBox ComboModel;
+        private System.Windows.Forms.Button clearList;
+        private System.Windows.Forms.CheckBox cmdPopCheck;
+        private System.Windows.Forms.CheckedListBox saveExtentionSelect;
+        private System.Windows.Forms.CheckBox autoClear;
+    }
+}
+
---
+++ whisperGUI/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IniParser.Model;
+using IniParser;
+
+
+
+namespace whisperGUI
+{
+    public partial class Form1 : Form
+    {
+
+        void SaveIni()
+        {
+            // create a new INI file parser
+            var parser = new FileIniDataParser();
+
+            // load the INI file
+            IniParser.Model.IniData iniData;
+            if (!File.Exists("config.ini"))
+            {
+                // create an empty ini data
+                iniData = new IniParser.Model.IniData();
+            }
+            else
+            {
+                // load the INI file
+                iniData = parser.ReadFile("config.ini");
+            }
+
+            // set the values of the sections
+            iniData["ComboLan"]["Value"] = (string)ComboLan.SelectedItem;
+            iniData["ComboModel"]["Value"] = (string)ComboModel.SelectedItem;
+            iniData["cmdPopCheck"]["Value"] = cmdPopCheck.Checked.ToString();
+            StringBuilder selectedItems = new StringBuilder();
+            foreach (var item in saveExtentionSelect.CheckedItems)
+            {
+                selectedItems.Append(item + ",");
+            }
+
+            iniData["saveExtentionSelect"]["Value"] = selectedItems.ToString();
+            iniData["autoclear"]["Value"] = autoClear.Checked.ToString();
+
+            // save the INI file
+            parser.WriteFile("config.ini", iniData);
+        }
+        void ReadIni()
+        {
+            // create a new INI file parser
+            var parser = new FileIniDataParser();
+
+            // load the INI file
+            IniParser.Model.IniData iniData;
+            if (!File.Exists("config.ini"))
+            {
+                // create an empty ini data
+                iniData = new IniParser.Model.IniData();
+
+            }
+            else
+            {
+                // load the INI file
+                iniData = parser.ReadFile("config.ini");
+            }
+            try
+            {
+                // load the INI file
+
+                string ComboLanValue = iniData["ComboLan"]["Value"];
+                if (string.IsNullOrWhiteSpace(ComboLanValue))
+                {
+                    ComboLanValue = "Chinese";
+                }
+                ComboLan.SelectedItem = ComboLanValue;
+
+                string ComboModelValue = iniData["ComboModel"]["Value"];
+                if (string.IsNullOrWhiteSpace(ComboModelValue))
+                {
+                    ComboModelValue = "medium";
+                }
+                ComboModel.SelectedItem = ComboModelValue;
+
+                string cmdPopCheckValue = iniData["cmdPopCheck"]["Value"];
+                if (string.IsNullOrWhiteSpace(cmdPopCheckValue))
+                {
+                    cmdPopCheckValue = "True";
+                }
+                cmdPopCheck.Checked = bool.Parse(cmdPopCheckValue);
+
+                string ext = iniData["saveExtentionSelect"]["Value"];
+                if (string.IsNullOrWhiteSpace(ext))
+                {
+                    ext = ".srt";
+                }
+                string[] selectedItems = ext.Split(',');
+                for (int i = 0; i < selectedItems.Length; i++)
+                {
+                    saveExtentionSelect.SetItemChecked(saveExtentionSelect.Items.IndexOf(selectedItems[i]), true);
+                }
+
+                string autoClearValue = iniData["autoclear"]["Value"];
+                if (string.IsNullOrWhiteSpace(autoClearValue))
+                {
+                    cmdPopCheckValue = "False";
+                }
+                autoClear.Checked = Convert.ToBoolean(autoClearValue);
+                // set the values to the corresponding controls
+
+                    
+                    
+            }
+            catch (Exception ex)
+            {
+                //在这里处理异常,例如显示错误信息给用户
+                MessageBox.Show(ex.Message);
+            }
+            if (!File.Exists("config.ini"))
+            {
+                // create an empty ini data
+                SaveIni();
+            }
+
+            // get the values of the sections
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.Columns.Add("FileName", "File Name"); // Create a new column named "FileName" and display name "File Name"
+            dataGridView1.Columns.Add("Status", "Status");  // Create a new column named "Status" and display name "Status"
+            dataGridView1.Columns[0].Width = 200;
+            ReadIni();
+            
+        }
+        List<string> filesToDealWith = new List<string>();
+        private void comboBox1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                comboBox1.Text = string.Join("/", openFileDialog1.FileName);
+            }
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            foreach (string file in openFileDialog1.FileNames)
+            {
+                filesToDealWith.Add(file);
+                string fileName = Path.GetFileName(file);
+                dataGridView1.Rows.Add(fileName, "Wait");
+            }
+        }
+        private void clearQueue_Click(object sender, EventArgs e)
+        {
+            filesToDealWith.Clear();
+                
+        }
+        string RemoveLastDot(string fileName)
+        {
+            int lastDotIndex = fileName.LastIndexOf(".");
+            if (lastDotIndex != -1)
+            {
+                fileName = fileName.Substring(0, lastDotIndex);
+            }
+            return fileName;
+        }
+        void clearSRT(string file,string BatFileName)
+        {
+            try
+            {
+                string fileName = Path.GetFileName(file);
+                string pureFileName = RemoveLastDot(fileName);
+                string[] extentions = { ".srt", ".vtt", ".txt" };
+                foreach (var extention in extentions)
+                {
+                    if (saveExtentionSelect.CheckedItems.Contains(extention))
+                    {
+                        string newFileName = pureFileName + extention;
+                        string oldFile = fileName + extention;
+                        string newFile = Path.Combine(Path.GetDirectoryName(file), newFileName);
+                        if (autoClear.Checked)
+                            File.Move(oldFile, newFile);
+                        else
+                            File.Copy(oldFile, newFile);
+                    }
+                    else
+                    {
+                        string oldFile = fileName + extention;
+                        if (autoClear.Checked)
+                            File.Delete(oldFile);
+                    }
+                }
+                if(autoClear.Checked)
+                    File.Delete(BatFileName);
+            }
+            catch (FileNotFoundException e)
+            {
+                // File not found, handle the exception
+                Console.WriteLine("The file was not found: " + e.Message);
+            }
+            catch (IOException e)
+            {
+                // An I/O error occurred, handle the exception
+                Console.WriteLine("An I/O error occurred: " + e.Message);
+            }
+        }
+        void ExecuteBatFile(string fileName)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = fileName;
+            if (cmdPopCheck.Checked)
+            {
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+            }
+            else
+            {
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            }
+            process.Start();
+            process.WaitForExit();
+        }
+        void UpdateStatus(string fileName, string status)
+        {
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[0].Value.ToString() == fileName)
+                {
+                    row.Cells[1].Value = status;
+                    row.Selected=true;
+                    break;
+                }
+            }
+        }
+        string modelQuality, SelectedLanguage;
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            
+            foreach (string file in filesToDealWith)
+            {
+                string fileName = Path.GetFileName(file);
+                string BatFileName = "whisper_" + fileName + ".bat";
+                List<string> lines = new List<string>();
+                lines.Add("whisper \"" + file + "\" --model "+ modelQuality+" --language "+ SelectedLanguage);
+                File.WriteAllLines(BatFileName, lines, Encoding.GetEncoding("GB2312"));
+                UpdateStatus(fileName, "In progress");
+                ExecuteBatFile(BatFileName);
+                clearSRT(file, BatFileName);
+                UpdateStatus(fileName, "Done");
+            }
+            filesToDealWith.Clear();
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            SaveIni();
+        }
+        private void clearList_Click(object sender, EventArgs e)
+        {
+
+            dataGridView1.Rows.Clear();
+            filesToDealWith.Clear();
+        }
+        private void ComboLan_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SelectedLanguage = ComboLan.Text;
+            SaveIni();
+        }
+
+        private void autoClear_CheckedChanged(object sender, EventArgs e)
+        {
+            SaveIni();
+        }
+
+        private void saveExtentionSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SaveIni();
+        }
+
+        private void ComboModel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            modelQuality = ComboModel.Text;
+            SaveIni();
+        }
+
+
+    }
+}
